8ebe770265c363b129cb76ee1cdcc34a70c47264
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 13:38:06 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -3,4 +3,4 @@ mycensus<-cleanCensus
 arrests<-USArrests
 arrest=cbind(rownames(arrests),arrests)
 colnames(arrests)=c("stateName")
-df<- merge(arrests,mycensus, )
+df<-merge(mycensus,arrests,by = c( "stateName"))
