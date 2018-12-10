b3ba57f418e923b506bf915e35b1bbee192a83da
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-10 00:24:16 -0500

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,14 +1,28 @@
 
-clean_data <- raw_data
-readStates<- function(states)
-{
-    states<- states[-1,]
-    num.row<-nrow(states)
-    states<-states[-num.row,]
-    states<-states[,-1:-4]
-    colnames(states)<-c("stateName","population","popOver18","percentOver18")
-    return(states)
-}  
-cleanCensus<- readStates(raw_data)
+#Read the census dataset 
+#We clean the dataset here in the function func1
 
-str(cleanCensus)
+func1 <- function(dfStates1)
+  {
+  #Clean the dataframe
+  #Removing the last Row(Puerto Rico). Using the '-' sign.
+  dfStates1<- dfStates1[-53,]
+  dfStates1
+  
+  #Removing the first row to remove the summary of United States row
+  dfStates1 <- dfStates1[-1,]
+  nrow(dfStates1)
+  dfStates1
+  
+  #Removing unwanted columns from the dataframe by using the '-' sign 
+  dfStates1 <- dfStates1[,-1:-4]
+  dfStates1
+  
+  #Renaming columns to simplify the data using the colname function
+  colnames(dfStates1) <- c("stateName","population","popOver18","percentOver18")
+    return(dfStates1)
+}
+
+func1 <- func1(raw_data)
+str(func1)
+
